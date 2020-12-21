fa7bf88f9d78ac77d2a8deb89918f5fe444363ff
cokia <hwymaster01@gmail.com>
2020-12-21 22:09:43 +0900

    add: generateToken

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
 import fetch from 'node-fetch';
-
-const toast_appkey = 'xjrdxV3H1NMEFdDb';
-const toast_secretkey = 'lliOwRne';
+import {ITokenGenerateBodyReq } from './interface'
 const toast_basic_endpoint = 'https://api-push.cloud.toast.com';
 const toast_push_check_endpoint = 'https://collector-push.cloud.toast.com';
+
+const toastPushApi = function (appkey, secretkey) {
+  this.appkey = key;
+  this.secretkey = secret;
+
+  _.bindAll(this);
+};
 
 fetch('https://github.com/')
   .then((res) => res.text())
@@ -28,29 +33,18 @@ fetch('https://httpbin.org/post', {
   .then((json) => console.log(json));
 
 // token
-// token generate
 
+// token generate
+const toastPushApi.token.generateToken = function(body:ITokenGenerateBodyReq){
 fetch('https://api-push.cloud.toast.com/push/v2.4/appkeys/${toast_appkey}/tokens', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json;charset=UTF-8',
   },
-  body: JSON.stringify({
-    oldToken: 'oldToken', // origin token (optional)
-    token: 'token', // token
-    isNotificationAgreement: true, // boolean
-    isAdAgreement: true, // boolean
-    isNightAdAgreement: true, // boolean
-    pushType: 'FCM', // enum 'FCM', 'APNS', 'APNS_SANDBOX', 'TENCENT', 'APNS_VOIP', 'APNS_SANDBOXVOIP', 'ADM'
-    timezoneId: 'Asia/Seoul', // IANA timezone style
-    uid: 'uid', // userid (max 64, no emoji)
-    country: 'KR', // ISO 3166
-    language: 'ko', // ISO 639-1
-    deviceId: 'deviceId', // device id (max 36)
-  }),
+  body: JSON.stringify(body),
 }).then((res) => res.json())
   .then((json) => console.log(json));
-
+}
 // RES
 //   {
 //     "header" : {
@@ -59,3 +53,22 @@ fetch('https://api-push.cloud.toast.com/push/v2.4/appkeys/${toast_appkey}/tokens
 //         "resultMessage" : "success"
 //     }
 // }
+
+// token read
+const toastPushApi.token.generateToken = function(token:string){
+  fetch('https://api-push.cloud.toast.com/push/v2.4/appkeys/${toast_appkey}/tokens/${token}?pushType=${PUSH_TYPE}', {
+    headers: {
+        'Content-Type': 'application/json;charset=UTF-8'
+    },
+  }).then((res) => res.json())
+    .then((json) => console.log(json));
+  }
+  // RES
+  //   {
+  //     "header" : {
+  //         "isSuccessful" : true,
+  //         "resultCode": 0,
+  //         "resultMessage" : "success"
+  //     }
+  // }
+  
