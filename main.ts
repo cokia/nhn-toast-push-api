a2c5855189b1e63e5b1154d5f1495d7483bc2e24
cokia <hwymaster01@gmail.com>
2020-12-22 17:36:03 +0900

    add: getTokenInfo method

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
 
@@ -16,17 +17,21 @@ export default class toastPushApi {
     this.secretKey = secret;
   }
 
-  // token generate
   async generateToken(body:ITokenGenerateBodyReq) {
-    await fetch(`${toastBasicEndpoint}${this.appKey}/tokens`, {
+    const response = await fetch(`${toastBasicEndpoint}${this.appKey}/tokens`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json;charset=UTF-8',
       },
       body: JSON.stringify(body),
-    }).then((res) => res.json())
-      .then((json) => console.log(json));
+    });
+    if (await response.status !== 200) {
+      return (`${await response.status}, ${await response.body} `);
+    }
+    const value = await response.json();
+    return value;
   }
+
   // RES
   //   {
   //     "header" : {
@@ -35,49 +40,43 @@ export default class toastPushApi {
   //         "resultMessage" : "success"
   //     }
   // }
+
+  // get token info
+  async getTokenInfo(pushType:string, token:string) {
+    const response = await fetch(`${toastBasicEndpoint}${this.appKey}/tokens/${token}?pushType=${pushType}`, {
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+    });
+    if (await response.status !== 200) {
+      return (`${await response.status}, ${await response.body} `);
+    }
+    const value = await response.json();
+    return value;
+  }
+
+  // RES
+  //   {
+  //     "token" : {
+  //         "pushType" : "FCM",
+  //         "isNotificationAgreement" : true,
+  //         "isAdAgreement" : true,
+  //         "isNightAdAgreement" : true,
+  //         "timezoneId" : "Asia/Seoul",
+  //         "country" : "KR",
+  //         "language" : "ko",
+  //         "uid" : "User ID",
+  //         "token" : "Token",
+  //         "updatedDateTime" : "2017-08-12T01:04:18.000+09:00",
+  //         "adAgreementDateTime" : "2017-08-12T01:04:19.000+09:00",
+  //         "nightAdAgreementDateTime" : "2017-08-12T01:04:19.000+09:00",
+  //         "deviceId" : "X3LOdJSQdNzCCvcbiSPZTGK1M9srPU5EumRD",
+  //         "activatedDateTime" : "2017-08-12T01:04:19.000+09:00"
+  //     },
+  //     "header" : {
+  //         "isSuccessful" : true,
+  //         "resultCode" : 0,
+  //         "resultMessage" : "success"
+  //     }
+  // }
 }
-
-// get token info
-// const toastPushApi.token.readToken = function(token:string,pushType:string){
-//   fetch('${toast_basic_endpoint}/push/v2.4/appkeys/${toast_appkey}/tokens/${token}?pushType=${pushType}', {
-//     headers: {
-//         'Content-Type': 'application/json;charset=UTF-8'
-//     },
-//   }).then((res) => res.json())
-//     .then((json) => console.log(json));
-//   }
-// RES
-//   {
-//     "token" : {
-//         "pushType" : "FCM",
-//         "isNotificationAgreement" : true,
-//         "isAdAgreement" : true,
-//         "isNightAdAgreement" : true,
-//         "timezoneId" : "Asia/Seoul",
-//         "country" : "KR",
-//         "language" : "ko",
-//         "uid" : "User ID",
-//         "token" : "Token",
-//         "updatedDateTime" : "2017-08-12T01:04:18.000+09:00",
-//         "adAgreementDateTime" : "2017-08-12T01:04:19.000+09:00",
-//         "nightAdAgreementDateTime" : "2017-08-12T01:04:19.000+09:00",
-//         "deviceId" : "X3LOdJSQdNzCCvcbiSPZTGK1M9srPU5EumRD",
-//         "activatedDateTime" : "2017-08-12T01:04:19.000+09:00"
-//     },
-//     "header" : {
-//         "isSuccessful" : true,
-//         "resultCode" : 0,
-//         "resultMessage" : "success"
-//     }
-// }
-
-// get Token info from UID
-// const toastPushApi.token.readTokenFromUID = function(uid:string){
-// fetch('${toast_basic_endpoint}/push/v2.4/appkeys/${APP_KEY}/tokens?uid=${uid}', {
-//     headers: {
-//         'Content-Type': 'application/json;charset=UTF-8',
-//         'X-Secret-Key': '${secretkey}'
-//     },
-//   }).then((res) => res.json())
-//     .then((json) => console.log(json));
-//   }
