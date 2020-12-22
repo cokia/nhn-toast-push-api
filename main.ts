bfc660799214098eafd2055628ecdea1458c8783
cokia <hwymaster01@gmail.com>
2020-12-22 15:33:43 +0900

    fix : change instance to class

--- main.ts
+++ main.ts
@@ -1,67 +1,28 @@
 import fetch from 'node-fetch';
-import {ITokenGenerateBodyReq } from './interface'
-const toast_basic_endpoint = 'https://api-push.cloud.toast.com';
-const toast_push_check_endpoint = 'https://collector-push.cloud.toast.com';
+import { ITokenGenerateBodyReq } from './interface';
 
-const toastPushApi = function (appkey, secretkey) {
-  this.appkey = key;
-  this.secretkey = secret;
+const toastBasicEndpoint = '${toast_basic_endpoint}';
+const toastPushCheckEndpoint = 'https://collector-push.cloud.toast.com';
+export default class toastPushApi {
+  appKey: string;
 
-  _.bindAll(this);
-};
+  secretKey: string;
 
-fetch('https://github.com/')
-  .then((res) => res.text())
-  .then((body) => console.log(body));
+  constructor(key:string, secret:string) {
+    this.appKey = key;
+    this.secretKey = secret;
+  }
 
-fetch('https://api.github.com/users/github')
-  .then((res) => res.json())
-  .then((json) => console.log(json));
-
-fetch('https://httpbin.org/post', { method: 'POST', body: 'a=1' })
-  .then((res) => res.json()) // expecting a json response
-  .then((json) => console.log(json));
-
-const body = { a: 1 };
-
-fetch('https://httpbin.org/post', {
-  method: 'post',
-  body: JSON.stringify(body),
-  headers: { 'Content-Type': 'application/json' },
-})
-  .then((res) => res.json())
-  .then((json) => console.log(json));
-
-// token
-
-// token generate
-const toastPushApi.token.generateToken = function(body:ITokenGenerateBodyReq){
-fetch('https://api-push.cloud.toast.com/push/v2.4/appkeys/${toast_appkey}/tokens', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json;charset=UTF-8',
-  },
-  body: JSON.stringify(body),
-}).then((res) => res.json())
-  .then((json) => console.log(json));
-}
-// RES
-//   {
-//     "header" : {
-//         "isSuccessful" : true,
-//         "resultCode": 0,
-//         "resultMessage" : "success"
-//     }
-// }
-
-// token read
-const toastPushApi.token.generateToken = function(token:string){
-  fetch('https://api-push.cloud.toast.com/push/v2.4/appkeys/${toast_appkey}/tokens/${token}?pushType=${PUSH_TYPE}', {
-    headers: {
-        'Content-Type': 'application/json;charset=UTF-8'
-    },
-  }).then((res) => res.json())
-    .then((json) => console.log(json));
+  // token generate
+  async generateToken(body:ITokenGenerateBodyReq) {
+    await fetch(`${toastBasicEndpoint}/push/v2.4/appkeys/${this.appKey}/tokens`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+      body: JSON.stringify(body),
+    }).then((res) => res.json())
+      .then((json) => console.log(json));
   }
   // RES
   //   {
@@ -71,4 +32,49 @@ const toastPushApi.token.generateToken = function(token:string){
   //         "resultMessage" : "success"
   //     }
   // }
-  
+}
+
+// get token info
+// const toastPushApi.token.readToken = function(token:string,pushType:string){
+//   fetch('${toast_basic_endpoint}/push/v2.4/appkeys/${toast_appkey}/tokens/${token}?pushType=${pushType}', {
+//     headers: {
+//         'Content-Type': 'application/json;charset=UTF-8'
+//     },
+//   }).then((res) => res.json())
+//     .then((json) => console.log(json));
+//   }
+// RES
+//   {
+//     "token" : {
+//         "pushType" : "FCM",
+//         "isNotificationAgreement" : true,
+//         "isAdAgreement" : true,
+//         "isNightAdAgreement" : true,
+//         "timezoneId" : "Asia/Seoul",
+//         "country" : "KR",
+//         "language" : "ko",
+//         "uid" : "User ID",
+//         "token" : "Token",
+//         "updatedDateTime" : "2017-08-12T01:04:18.000+09:00",
+//         "adAgreementDateTime" : "2017-08-12T01:04:19.000+09:00",
+//         "nightAdAgreementDateTime" : "2017-08-12T01:04:19.000+09:00",
+//         "deviceId" : "X3LOdJSQdNzCCvcbiSPZTGK1M9srPU5EumRD",
+//         "activatedDateTime" : "2017-08-12T01:04:19.000+09:00"
+//     },
+//     "header" : {
+//         "isSuccessful" : true,
+//         "resultCode" : 0,
+//         "resultMessage" : "success"
+//     }
+// }
+
+// get Token info from UID
+// const toastPushApi.token.readTokenFromUID = function(uid:string){
+// fetch('${toast_basic_endpoint}/push/v2.4/appkeys/${APP_KEY}/tokens?uid=${uid}', {
+//     headers: {
+//         'Content-Type': 'application/json;charset=UTF-8',
+//         'X-Secret-Key': '${secretkey}'
+//     },
+//   }).then((res) => res.json())
+//     .then((json) => console.log(json));
+//   }
